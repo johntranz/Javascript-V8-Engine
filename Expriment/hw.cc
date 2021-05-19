dd910d4f7d9649f514e1d01be6afb9635bdd5cfb
Huy Tran <quanghuy2621998@gmail.com>
2021-05-19 17:07:17 +0700

    Example: add two number agrument form javascript to C++ handle

--- Expriment/hw.cc
+++ Expriment/hw.cc
@@ -1,9 +1,11 @@
 #include <node.h>
 
 namespace hw {
+    using v8::Exception;
     using v8::FunctionCallbackInfo;
     using v8::Isolate;
     using v8::Local;
+    using v8::Number;
     using v8::Object;
     using v8::String;
     using v8::Value;
@@ -13,8 +15,18 @@ namespace hw {
         args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello World !"));
     }
 
+    void Sum(const FunctionCallbackInfo<Value>& args) {
+        Isolate* isolate = args.GetIsolate();
+        
+        double total = args[0].As<Number>()->Value() + args[1].As<Number>()->Value();
+        Local<Number> num = Number::New(isolate, total);
+
+        args.GetReturnValue().Set(num);
+    }
+
     void init(Local<Object> exports) {
         NODE_SET_METHOD(exports, "hw", Method);
+        NODE_SET_METHOD(exports, "sum", Sum);
     }
 
     NODE_MODULE(NODE_GYP_MODULE_NAME, init)
--- Expriment/index.js
+++ Expriment/index.js
@@ -1,3 +1,4 @@
 var addon = require('./build/Release/addon')
 
 console.log(addon.hw())
+console.log(addon.sum(3.5, 4.12343434333333333334))
